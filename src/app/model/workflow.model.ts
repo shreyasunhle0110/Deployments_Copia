6fd9319b61acb9e4b9f45977b3fa2b05a5fb78c0
shubham261996 <tagadechinmay@gmail.com>
2021-06-24 09:58:47 +0530

    Added validation in login page and implemented add user functionality in create workflow page

--- src/app/model/workflow.model.ts
+++ src/app/model/workflow.model.ts
@@ -14,3 +14,19 @@ export class WorkflowRegisterModel {
     sendEmailWithAuthCode: boolean;
     AuthorizationCode: string;
 }
+
+export class SysbrijUserModel {
+    name: string;
+    email: string;
+    address: string;
+    mobile: string;
+    role: string;
+
+    constructor(name, email, address, mobile, role) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.mobile = mobile;
+        this.role = role;
+    }
+}
--- src/app/sysbrij-start-new-deployment/sysbrij-start-new-deployment.component.ts
+++ src/app/sysbrij-start-new-deployment/sysbrij-start-new-deployment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkflowService } from '../services/workflow.service';
-import { WorkflowRegisterModel } from '../model/workflow.model';
+import { WorkflowRegisterModel, SysbrijUserModel } from '../model/workflow.model';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-sysbrij-start-new-deployment',
@@ -9,23 +10,51 @@ import { WorkflowRegisterModel } from '../model/workflow.model';
 })
 export class SysbrijStartNewDeploymentComponent implements OnInit {
   workflowRegisterModel: WorkflowRegisterModel;
-  constructor(private workflowService:WorkflowService) {
+  sysbrijUserListModel: SysbrijUserModel[] = [];
+  loaderActive:boolean = false;
+  monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  constructor(private workflowService:WorkflowService, private router: Router) {
     this.workflowRegisterModel = new WorkflowRegisterModel();
    }
 
   ngOnInit(): void {
+    var registerDate = new Date();
+    this.workflowRegisterModel.date = registerDate.getDate() + " " + this.monthNames[registerDate.getMonth()] + " " + registerDate.getFullYear();
   }
 
   workflowRegister() {
-    debugger;
+    this.loaderActive = true;
     this.workflowRegisterModel.status= "1";
     this.workflowRegisterModel.createdBy= "1";
     this.workflowService.workflowRegister(this.workflowRegisterModel).subscribe(
       (response: any) => {
         debugger;
-        console.log("response received");
-        console.log(response);
+        if(response.Result == true) {
+          this.loaderActive = false;
+          alert("Company Register Successfull!");
+          this.router.navigate(["/sysbrijMaster/sysbrijMyWorkflows"]);
+        }
+        else {
+          this.loaderActive = false;
+          alert("Error!");
+        }
       })
   }
 
+  addSysbrijUser(userName, userEmail, userAddress, userMobile, userRole) {
+    debugger;
+    this.sysbrijUserListModel.push(
+      new SysbrijUserModel(userName.value, userEmail.value, userAddress.value, userMobile.value, userRole.value)
+    )
+
+    userName.value = "";
+    userEmail.value = "";
+    userAddress.value = "";
+    userMobile.value = "";
+    userRole.value = "";
+  }
+
+  removeUser(index) {
+    this.sysbrijUserListModel.splice(index, 1);
+  }
 }
